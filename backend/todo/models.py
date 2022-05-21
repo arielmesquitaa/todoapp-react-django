1896895d0b70351ab585a5e69847d117c8f9204f
Ariel Mesquita <arielmesquita@live.com>
2022-05-21 17:05:28 -0300

    model ajust

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -12,7 +12,8 @@ class Todo(models.Model):
     created_at = models.DateTimeField(auto_now=True)
 
     # user who posted the
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, default=User)
 
     def __str__(self):
         return self.title
