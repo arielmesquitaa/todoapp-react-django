2efd5fca5b901162229ffd744f9f4ae9e7328499
Ariel Mesquita <arielmesquita@live.com>
2022-05-22 14:21:45 -0300

    adding POST requests to the endpoint todos/

--- backend/api/views.py
+++ backend/api/views.py
@@ -10,3 +10,14 @@ class TodoList(generics.ListAPIView):
     def get_queryset(self):
         user = self.request.user
         return Todo.objects.filter(user=user).order_by('-created')
+
+
+class TodoListCreate(generics.ListCreateAPIView):
+    serializer_class = TodoSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user).order_by('-created')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
